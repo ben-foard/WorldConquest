62be907715c11478fb4136ebb5348f1690f4b1f7
tb1819x <109041922+tb1819x@users.noreply.github.com>
2024-02-20 17:23:28 +0000

    skeleton code
    Committing completed skeleton code for sprint_one

---
+++ world_conquest/Assets/Scripts/player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //string name;
+    //int troopCount = 5;
+    /**
+        public Player(String name)
+        {
+            this.name = name;
+        }
+    **/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+    
+    /**addtroops()
+        //skips for the moment
+    **/
+
+
+    /**attackcountry()
+        //player1 attacks player2 
+        //for this phase player2 loses troops on each turn
+    **/
+
+    /**fortify()
+        //for this sprint this phase will just print fortified 
+    **/
+
+    /**gettroopsTotal()
+        //skips for the moment
+    **/
+
+    /**getPlayerNamel()
+        //skips for the moment
+    **/
+}
---
+++ world_conquest/Assets/Scripts/territory.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+ //Name
+    //Sections = 1 
+    //troopPerSection = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /**setTroops()
+        this.troopsPerland = random assigned.
+    **/
+
+    /**getTroops()
+    **/
+
+    /**getName()
+
+    **/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
