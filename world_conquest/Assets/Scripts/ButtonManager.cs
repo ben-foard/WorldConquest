9b78cabdceb06d884130f26fb74d52bca9fd5f7a
Owen <owenjgibson@gmail.com>
2024-02-21 20:52:40 +0000

    Fixed compiler GameManager errors

--- world_conquest/Assets/Scripts/ButtonManager.cs
+++ world_conquest/Assets/Scripts/ButtonManager.cs
@@ -2,27 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class ButtonManager : MonoBehaviour
 {
     void Start()
     {
-        // Initialize button properties, such as visibility based on game state
+        
     }
 
     void Update()
     {
-        // Check for input or state changes to update button state
+        
     }
 
-    public void attackButton()
+    public void AttackButton()
     {
-        // Verify attack phase, select countries, and initiate attack
+        
     }
-
-    public void fortifyButton()
-    {
-        // Allow player to move troops between countries during fortify phase
-    }
-    
+    //public void
 }
-
